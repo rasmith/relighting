49dbbd8862a63cab982a01b1fa0432b67388df5e
rasmith <rsmith0x0@gmail.com>
2018-11-02 00:45:46 -0500

    slight modifation to training_and_evalute_tasks.py

--- train_and_evaluate_tasks.py
+++ train_and_evaluate_tasks.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python 
 from evaluator import Evaluator
 from trainer import Trainer
 import importlib
@@ -9,14 +10,20 @@ device = 'cpu'
 
 for importer, modname, ispkg in pkgutil.iter_modules(package.__path__):
     print('--------------------------------------------')
+    print(f'Task:{modname}')
     target_task = importlib.import_module(f'{package.__name__}.{modname}')
     enabled = target_task.cfg['enabled']
     data_wrapper = target_task.cfg['data_wrapper']
     if enabled:
+        print(f'{modname} is enabled ... training.')
         cfg_loader = target_task.CfgLoader()
         t = Trainer(cfg_loader, data_wrapper)
         t.init(device)
         t.train()
+        print('Evaluating..')
         e = Evaluator(cfg_loader, data_wrapper)
         e.init(device)
         e.evaluate()
+    else:
+        print(f'{modname} is disabled ... skipping.')
+
