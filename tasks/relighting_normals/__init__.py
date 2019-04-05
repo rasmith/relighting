7bf6672bb43a5e29fdb15e26e66e8f80fc21658d
rasmith <rsmith0x0@gmail.com>
2019-04-05 14:28:04 -0500

    relighting normals tasks

--- tasks/relighting_normals/__init__.py
+++ tasks/relighting_normals/__init__.py
@@ -13,27 +13,39 @@ import torch
 task_name = 'relighting_normals'
 
 cfg = {
-    'batch_size' : 8,
+    'annealing_step' : 1000,
+    # 'batch_size' : 32,
+    'batch_size' : 32,
+    'base_steps' : int(128/32),
     'cfg_file' : 'config.cfg',
     'criterion' : nn.MSELoss(),
-    'data_wrapper' : (lambda x : Subset(x, range(32))),
-    'data_wrapper' : None,
+    'criterion_gan': nn.MSELoss(),
+    'criterion_pixel_l1': nn.L1Loss(),
+    # 'data_wrapper' : None,
+    'data_wrapper' : (lambda x : Subset(x, range(128))),
     'dc_img' : f'dc_img/{task_name}',
     # 'enabled' : True,
     'enabled' : False,
     'eval_dir':f'eval/{task_name}',
-    'image_dir' : f'sources/{task_name}',
-    'input_dir' : '.',
-    'learning_rate' : 1e-3,
+    'evaluation_enabled': True, # to evaluate this task
+    'image_dir' : 'sources/{task_name}', # source "images"
+    'input_dir' : '.', # base input for all data needed
+    'lambda_pixel': 100,
+    'learning_rate' : 1e-4,
+    'learning_rate_discriminator' : 1e-4,
+    'log_to_tensorboard': True,
+    'num_epochs' : 100,
     # 'num_epochs' : 2,
-    'num_epochs' : 200,
+    'phases': ['training', 'validation'],
     'seed': (lambda : 42),
     'shuffle': True,
     'target_dir': f'out',
     'target_transform' : Compose([ToTensor(), Normalize((0.5,), (1.0,))]),
     'task_name': f'{task_name}',
+    'trainer' : 'gan',
+    'training_enabled': True, # to train this task
     'transform' : None,
-    'use_sampler': True, 
+    'use_sampler': False, 
     'validation_split': .2,
     'weight_decay': 1e-5,
     'weights_file' : f'weights/{task_name}.pth'
@@ -44,29 +56,46 @@ class CfgLoader(object):
     self.cfg = cfg
 
   def get_cfg(self, device):
-    self.cfg['activation'] = nn.Tanh()
-    self.cfg['model'] = r.Conv11Decoder128x128(self.cfg['activation']).cuda(device) \
-      if "cuda" in device else r.Conv11Decoder128x128(self.cfg['activation']).cpu()
+    self.cfg['device'] = device
     self.cfg['dataset'] = ContextVectorDataset(cfg['input_dir'], cfg['cfg_file'],
                                             cfg['image_dir'], cfg['target_dir'],
                                             cfg['task_name'], cfg['transform'],
                                             cfg['target_transform'])
+    if self.cfg['data_wrapper'] is not None:
+      self.cfg['wrapped_dataset'] = self.cfg['data_wrapper'](self.cfg['dataset'])
+    else :
+      self.cfg['wrapped_dataset'] = self.cfg['dataset']
+    self.cfg['activation'] = nn.Tanh()
+    self.cfg['model'] = r.Conv11Decoder128x128()
+    self.cfg['discriminator'] = r.Discriminator()
+    if device in 'cuda':
+      self.cfg['model'] = self.cfg['model'].cuda()
+      self.cfg['discriminator'] = self.cfg['discriminator'].cuda()
+      self.cfg['criterion'] = self.cfg['criterion'].cuda()
+      self.cfg['criterion_pixel_l1'] = self.cfg['criterion_pixel_l1'].cuda()
+      self.cfg['criterion_gan'] = self.cfg['criterion_gan'].cuda()
     self.cfg['optimizer'] = Adam(cfg['model'].parameters(),
                                  lr=cfg['learning_rate'],
                                  weight_decay=cfg['weight_decay'])
+    self.cfg['optimizer_discriminator'] = \
+      Adam(cfg['discriminator'].parameters(), lr=cfg['learning_rate_discriminator'],
+            weight_decay =  cfg['weight_decay_discriminator'])
     if self.cfg['use_sampler']:
       self.cfg['shuffle'] = False
-      if self.cfg['data_wrapper'] is not None:
-        dataset_size = len(self.cfg['data_wrapper'](self.cfg['dataset']))
-      else:
-        dataset_size = len(self.cfg['dataset'])
-
-      indices = list(range(dataset_size))
-      split = int(np.floor(self.cfg['validation_split'] * dataset_size))
-      np.random.seed(self.cfg['seed']())
-      np.random.shuffle(indices)
-      train_indices, validation_indices = indices[split:], indices[:split]
-      self.cfg['train_sampler'] = SubsetRandomSampler(train_indices)
-      self.cfg['validation_sampler'] = SubsetRandomSampler(validation_indices)
+    dataset_size = len(self.cfg['wrapped_dataset'])
+    indices = list(range(dataset_size))
+    split = int(np.floor(self.cfg['validation_split'] * dataset_size))
+    np.random.seed(self.cfg['seed']())
+    np.random.shuffle(indices)
+    training_indices, validation_indices = indices[split:], indices[:split]
+    self.cfg['indices'] = indices
+    self.cfg['training_indices'] = training_indices
+    self.cfg['validation_indices'] = validation_indices
+    self.cfg['training_dataset'] = Subset(self.cfg['wrapped_dataset'], \
+                                          training_indices)
+    self.cfg['validation_dataset'] = Subset(self.cfg['wrapped_dataset'], \
+                                          validation_indices)
+    self.cfg['training_sampler'] = None
+    self.cfg['validation_sampler'] = None
     return self.cfg
 
