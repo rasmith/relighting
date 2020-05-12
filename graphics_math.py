fix quaternion code, sign error on axis

graphics_math.py
@@ -1,64 +1,93 @@
 import numpy as np
+from scipy.spatial.transform import Rotation as R
+
 
 def perspective(fovy, aspect, near, far):
-  f = 1.0 / np.tan(fovy * np.pi / 360.0);
-  d = near - far;
-  matrix = np.zeros((4, 4))
-  matrix[0,0]= f / aspect
-  matrix[1,1] = f
-  matrix[2,2] = (near + far) / d
-  matrix[2,3] = 2.0 * near * far / d
-  matrix[3,2] = -1.0
-  return matrix
+    f = 1.0 / np.tan(fovy * np.pi / 360.0)
+    d = near - far
+    matrix = np.zeros((4, 4))
+    matrix[0, 0] = f / aspect
+    matrix[1, 1] = f
+    matrix[2, 2] = (near + far) / d
+    matrix[2, 3] = 2.0 * near * far / d
+    matrix[3, 2] = -1.0
+    return matrix
+
 
 def lookat(eye, at, up):
-  z_direction = eye[0:3, 0] - at[0:3, 0]
-  z_direction = z_direction / np.linalg.norm(z_direction)
-  x_direction = np.cross(up[0:3, 0] / np.linalg.norm(up[0:3, 0]), z_direction)
-  x_direction = x_direction / np.linalg.norm(x_direction)
-  y_direction = np.cross(z_direction, x_direction)
-  y_direction = y_direction / np.linalg.norm(y_direction)
-  matrix = np.eye(4)
-  matrix[0, 0:3] = x_direction
-  matrix[1, 0:3] = y_direction
-  matrix[2, 0:3] = z_direction
-  matrix[0:3, 3] = -matrix[0:3,0:3].dot(eye[0:3,0])
-  return matrix
+    z_direction = eye[0:3, 0] - at[0:3, 0]
+    z_direction = z_direction / np.linalg.norm(z_direction)
+    x_direction = np.cross(up[0:3, 0] / np.linalg.norm(up[0:3, 0]), z_direction)
+    x_direction = x_direction / np.linalg.norm(x_direction)
+    y_direction = np.cross(z_direction, x_direction)
+    y_direction = y_direction / np.linalg.norm(y_direction)
+    matrix = np.eye(4)
+    matrix[0, 0:3] = x_direction
+    matrix[1, 0:3] = y_direction
+    matrix[2, 0:3] = z_direction
+    matrix[0:3, 3] = -matrix[0:3, 0:3].dot(eye[0:3, 0])
+    return matrix
+
 
 def rotate(angle, axis):
-  x, y, z = axis.transpose()[0, :]
-  K = np.array([[0, -z, y], [z, 0, -x], [-y, x, 0]], dtype = np.float)
-  I = np.eye(3, 3, dtype = np.float)
-  R = I + np.sin(angle) * K + (1.0 - np.cos(angle)) * K.dot(K)
-  matrix = np.eye(4, 4, dtype = np.float)
-  matrix[0:3, 0:3] = R
-  return matrix
+    # if isinstance(axis, np.ndarray):
+        # axis = axis.flatten().tolist()
+    # if isinstance(axis, list) and len(axis) >= 3:
+        # x, y, z = axis[:3]
+    # r = R.from_rotvec(angle * np.array(axis))
+    # print(r)
+    # matrix = np.eye(4,4, dtype = np.float)
+    # matrix[0:3, 0:3] = r[0]
+    # return matrix
+    if isinstance(axis, np.ndarray):
+        axis = axis.flatten().tolist()
+    if isinstance(axis, list) and len(axis) >= 3:
+        x, y, z = axis[:3]
+    else:
+        print(f'axis = {axis}')
+        raise f"Need type to be list or ndarray and have at least 3 values."
+    mag = np.sqrt(x * x + y * y + z * z)
+    np.seterr(all='raise')
+    x /= mag
+    y /= mag
+    z /= mag
+    K = np.array([[0, -z, y], [z, 0, -x], [-y, x, 0]], dtype=np.float)
+    I = np.eye(3, 3, dtype=np.float)
+    R = I + np.sin(angle) * K + (1.0 - np.cos(angle)) * K.dot(K)
+    matrix = np.eye(4, 4, dtype=np.float)
+    matrix[0:3, 0:3] = R
+    return matrix
 
 def quaternion(m):
-  cos_theta = 0.5 * (np.trace(m) - 1)
-  sin_theta = np.sqrt(1.0 - cos_theta * cos_theta)
-  theta = np.arccos(cos_theta)
-  x = 0.5 * m[2, 1] - m[1, 2] / sin_theta
-  y = 0.5 * m[0, 2] - m[2, 0] / sin_theta
-  z = 0.5 * m[1, 0] - m[0, 1] / sin_theta
-  v = np.array([x, y, z])
-  v = v / np.linalg.norm(v)
-  c = np.cos(0.5 * theta)
-  s = np.sin(0.5 * theta)
-  q = np.array([c, v[0] * s, v[1] * s, v[2] * s])
-  return q
-  
+    if isinstance(m, list):
+        m = np.reshape(np.array(m[:9], dtype=np.float32, order = 'C'), (3, 3))
+    r = R.from_matrix(m[:3, :3])
+    q = r.as_quat()
+    return np.array([q[3], q[0], q[1], q[2]])
+    return q
+
+def quaternion_to_matrix(q):
+    mag = np.linalg.norm(q[1:4])
+    if mag == 0.0:
+        angle = 0
+        axis = [1, 0, 0]
+    else:
+        angle = 2.0 * np.arctan2(mag, q[0])
+        axis = q[1:4] / mag
+    # print(f'q2m: q={q},  angle = {angle}, axis = {axis}')
+    return rotate(angle, axis)
+
 def uniform_scale(value):
-  scale_matrix = np.eye(4)
-  scale_matrix[0, 0] = value
-  scale_matrix[1, 1] = value
-  scale_matrix[2, 2] = value
-  return scale_matrix
+    scale_matrix = np.eye(4)
+    scale_matrix[0, 0] = value
+    scale_matrix[1, 1] = value
+    scale_matrix[2, 2] = value
+    return scale_matrix
 
 
 def translate(tx, ty, tz):
-  translate_matrix = np.eye(4)
-  translate_matrix[0, 3] = tx
-  translate_matrix[1, 3] = ty
-  translate_matrix[2, 3] = tz
-  return translate_matrix
+    translate_matrix = np.eye(4)
+    translate_matrix[0, 3] = tx
+    translate_matrix[1, 3] = ty
+    translate_matrix[2, 3] = tz
+    return translate_matrix

image_view.py
@@ -19,10 +19,20 @@ class ImageModel(GlfwModel):
     def initialize(self):
         if self.provider_type is str:
             self.image_path = provider
-            self.image = io.imread(self.image_path)
+            self.imag_value  = io.imread(self.image_path)
         elif self.provider_type is np.ndarray:
-            self.image = self.provider
-        self.image_byte_count = ArrayDatatype.arrayByteCount(self.image)
+            self.image_value  = self.provider
+        elif callable(self.provider):
+            self.image_value = self.provider()
+
+        self.image_byte_count = ArrayDatatype.arrayByteCount(self.image_value)
+
+    @property
+    def image(self):
+        if callable(self.provider):
+            return self.provider()
+        return self.image_value
+
 
 class ImageView(GlfwView):
     def __init__(self, fragment_shader_path, vertex_shader_path):
@@ -47,6 +57,7 @@ class ImageView(GlfwView):
           ]).astype(dtype=np.float32, order='C')
         self.texture_byte_count = ArrayDatatype.arrayByteCount(self.texture_data)
         self.image_loaded = False
+        self.image_storage_initialized = False 
 
     def update_vbos(self):
         glBindVertexArray(self.vao_id)
@@ -146,8 +157,12 @@ class ImageView(GlfwView):
         glBindTexture(GL_TEXTURE_2D, self.texture_id)
 
         image_width, image_height, image_channels = self.model.image.shape
-        if not self.image_loaded:
-            glTexStorage2D(GL_TEXTURE_2D, 1, GL_RGB8, image_width, image_height)
+        if not self.image_loaded or callable(self.model.provider):
+            if not self.image_storage_initialized:
+                glTexStorage2D(GL_TEXTURE_2D, 1, GL_RGB8, image_width, image_height)
+                self.image_storage_initialized = True
+            print(f"Get the image, pose = {self.model.provider.current_pose}.")
+            glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
             glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, image_width, image_height,
                             GL_RGB, GL_UNSIGNED_BYTE, self.model.image)
             self.image_loaded = True

visualize.py
@@ -54,6 +54,8 @@ def to_numpy_img(x):
 
 
 def to_torch_pose(x):
+    # import pdb
+    # pdb.set_trace()
     x = torch.from_numpy(np.reshape(x, (1, 1, 1, 7)).astype(np.float32))
     x = Variable(x).cpu()
     return x
@@ -143,6 +145,8 @@ C, res, rank, svals = np.linalg.lstsq(A.T, f.T, rcond=None)
 radius = (np.linalg.norm(C[0:3]) ** 2 + C[3]) ** (1 / 2)
 print(f"C = {C}, R = {((np.linalg.norm(C[0:3]) ** 2) + C[3]) ** (1/2)}")
 print(f"C[0] = {C[0]}, C[1] = {C[1]}, C[2] = {C[2]}")
+sample_center = C
+sample_radius = radius
 
 app = GlfwApp()
 app.init()
@@ -219,6 +223,22 @@ class KeyCallbackHandler:
         obj = self.data.name_to_mesh_info[name]
         obj["M"] = np.linalg.multi_dot([obj["T"], obj["R"], obj["scale"]])
 
+    def render_current(self, name):
+        obj = self.data.name_to_mesh_info[name]
+        M = obj["M"]
+        q = gm.quaternion(M)
+        position = M[0:3, 3]
+        x, y, z  = position[0], position[1], position[2]
+        x, y, z = 2 * x, 2 * y, 2 * z
+        X = sample_center[0,0] + sample_radius[0] * x
+        Y = sample_center[1,0] + sample_radius[0] * y 
+        Z = sample_center[2,0] + sample_radius[0] * z 
+        provider.pose = [X, Y, Z, q[0], q[1], q[2], q[3]]
+        print("Render!")
+        print(C)
+        print(f"x = {x}, y = {y}, z = {z}")
+        
+
     def key_handler(self, key, scancode, action, mods):
         if key == glfw.KEY_W and action == glfw.PRESS:
             self.update_translation("axes", 0, 0, 0.025)
@@ -245,9 +265,31 @@ class KeyCallbackHandler:
         elif key == glfw.KEY_L and action == glfw.PRESS:
             self.update_orientation("axes", 2, -0.25)
         elif key == glfw.KEY_SPACE and action == glfw.PRESS:
-            print("Render!")
+            self.render_current("axes")
         obj = self.data.name_to_mesh_info["axes"]
         self.update_model_matrix("axes")
+
+
+class ImageProvider:
+    def __init__(self, model):
+        self.model = model
+        self.current_image = None
+
+    def __call__(self):
+        return self.current_image
+
+    @property
+    def pose(self):
+        return self.current_pose
+
+    @pose.setter
+    def pose(self, new_pose):
+        self.current_pose = new_pose 
+        if self.current_image is None:
+            self.current_image = to_numpy_img(self.model(to_torch_pose(new_pose)))
+        else:
+            rendered_image = to_numpy_img(self.model(to_torch_pose(new_pose)))
+            np.copyto(self.current_image, rendered_image)
 
 
 multi_mesh_view = MultiMeshView()
@@ -270,12 +312,10 @@ multi_controller.add(mesh_controller)
 image_fragment_shader = "image_fragment.glsl"
 image_vertex_shader = "image_vertex.glsl"
 
-output_image = (
-    to_numpy_img(model(to_torch_pose(poses[0])))
-    if load_model
-    else checker_board()
-)
-
+provider = ImageProvider(model) if load_model else None
+if load_model:
+    provider.pose = poses[0]
+output_image = provider if load_model else checker_board()
 image_model = ImageModel(output_image)
 image_view = ImageView(image_fragment_shader, image_vertex_shader)
 